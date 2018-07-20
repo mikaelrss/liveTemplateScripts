2bd89962ea8514725c2c78ae439b1895d64545d3
Mikael Rino Solstad <mikael.solstad@systek.no>
2018-07-20 21:43:22 +0200

    Added script for generating defaultProps object in React test files from flow-typed Props-type

---
+++ defaultPropsGenerator.groovy
@@ -0,0 +1,14 @@
+Random random = new Random()
+String test = _1.toString().replaceAll(/.test./, '.')
+
+String fileContent = new File(test).text
+def result = (fileContent =~ /(Props = [ a-zA-Z?:,(){}<>=\n]*)/)[0][1]
+def props = result.toString().split(/Props = /)[1] + ";"
+
+return props.replaceAll(/: string/, ": 'testString'")
+        .replaceAll(/: number/, ": " + random.nextInt(100).toString())
+        .replaceAll(/Array<Array<[a-zA-z]*>>/, "[[{}],[{}]]")
+        .replaceAll(/Array<[a-zA-z<>]*>/, "[{}]")
+        .replaceAll(/: Function/, ": jest.fn()")
+        .replaceAll(/: \([a-zA-Z:<> ']*\) => Promise<[a-zA-Z)]*>/, ": jest.fn().mockImplementationOnce(() => Promise.resolve())")
+        .replaceAll(/: \([a-zA-Z:<> ]*\) => [a-zA-Z<>]*/, ": jest.fn()")
