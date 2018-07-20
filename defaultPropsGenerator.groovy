b1418e236f21e23b3b22ea8f8f79f4827bf6e637
Mikael Rino Solstad <mikael.solstad@systek.no>
2018-07-20 22:30:08 +0200

    Added support for arrays of type number

--- defaultPropsGenerator.groovy
+++ defaultPropsGenerator.groovy
@@ -7,8 +7,10 @@ def props = result.toString().split(/Props = /)[1] + ";"
 
 return props.replaceAll(/: string/, ": 'testString'")
         .replaceAll(/: number/, ": " + random.nextInt(100).toString())
-        .replaceAll(/Array<Array<[a-zA-z]*>>/, "[[{}],[{}]]")
+        .replaceAll(/Array<number>/, "[" + random.nextInt(100) + "," + random.nextInt(100) + "]")
+        .replaceAll(/Array<Array<[a-zA-Z]*>>/, "[[{}],[{}]]")
         .replaceAll(/Array<[a-zA-z<>]*>/, "[{}]")
+        .replaceAll(/Array<[0-9\[\],]*>/, "[[" + random.nextInt(100) + "], [" + random.nextInt(100) + "]]")
         .replaceAll(/: Function/, ": jest.fn()")
         .replaceAll(/: \([a-zA-Z:<> ']*\) => Promise<[a-zA-Z)]*>/, ": jest.fn().mockImplementationOnce(() => Promise.resolve())")
         .replaceAll(/: \([a-zA-Z:<> ]*\) => [a-zA-Z<>]*/, ": jest.fn()")
