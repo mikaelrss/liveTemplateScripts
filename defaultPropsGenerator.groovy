06ebc265867c92255a436636a8c47ea5ae6fcb23
Mikael Rino Solstad <mikael.solstad@systek.no>
2018-07-20 22:14:13 +0200

    Added support for numbers in prop names in defaultPropGenerator

--- defaultPropsGenerator.groovy
+++ defaultPropsGenerator.groovy
@@ -2,7 +2,7 @@ Random random = new Random()
 String test = _1.toString().replaceAll(/.test./, '.')
 
 String fileContent = new File(test).text
-def result = (fileContent =~ /(Props = [ a-zA-Z?:,(){}<>=\n]*)/)[0][1]
+def result = (fileContent =~ /(Props = [ a-zA-Z0-9?:,(){}<>=\n]*)/)[0][1]
 def props = result.toString().split(/Props = /)[1] + ";"
 
 return props.replaceAll(/: string/, ": 'testString'")
